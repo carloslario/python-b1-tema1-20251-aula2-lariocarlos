704ffd96715a935560fc595f51216f5c6b17b9d6
carloslario <carloslariogomez@gmail.com>
2025-12-10 19:40:11 +0100

    ejercicio 2 subido

--- 1a/ej1a2.py
+++ 1a/ej1a2.py
@@ -19,32 +19,19 @@ Ejemplo:
     Salida:
     30
 
-Enunciat:
-Crea una funció 'sum_odd_numbers(list_numbers)' que rebi com
-paràmetre una llista de nombres positius enters anomenada 'list_numbers'
-i torneu la suma dels números imparells trobats a la llista.
-Considereu que 'list_numbers' ha de contenir valors numèrics enters majors
-o iguals a '0', en cas contrari cal mostrar un error tipus 'ValueError'.
-
-L'error el pots mostrar amb la següent instrucció:
-raise ValueError("MISSATGE D'ERROR")
-
-Paràmetres:
-- list_numbers: Llista de nombres enters positius.
-
-Exemple:
-     Entrada:
-     sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100])
-
-     Sortida:
-     30
-
 '''
 
 def sum_odd_numbers(list_numbers):
-    # Write here your code
-    pass
+    sum=0
+    for n in list_numbers:
+        if not isinstance(n, int):
+            raise ValueError("Number in list must be integer.")
+        elif n < 0:
+            raise ValueError("Number must be greater or equal than 0.")
+        if n%2 != 0:
+            sum +=n
+    return sum
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))
+# se trata de sumar los numeros impares revisando que no haya problemas 
+print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))

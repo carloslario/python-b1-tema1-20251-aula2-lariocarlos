1edf389b46cdf0374f8a3607cfbbb43aca3a7903
carloslario <carloslariogomez@gmail.com>
2025-12-10 20:55:09 +0100

    ejercicio 1b subido

--- 1b/ej1b1.py
+++ 1b/ej1b1.py
@@ -15,29 +15,12 @@ Ejemplo:
 
     Salida:
     87
-
-Enunciat:
-Implementar la funció 'obtain_max(list_numbers)' que rep 
-com a paràmetre una llista no buida de nombres enters i retorna
-el número major de la llista.
-
-Recorda que en Python existeix la funció anomenada 'max'
-
-Paràmetres:
-- list_numbers: Llista de nombres enters
-
-Exemple:
-    Entrada:
-    obtain_max([1, 45, 87, 21, 0, 23, 28])
-
-    Sortida:
-    87
 """
 
 def obtain_max(list_numbers):
-    # Write here your code
-    pass
+    maximum = max(list_numbers)    
+    return maximum
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(obtain_max([1, 45, 87, 21, 0, 23, 28]))
+#la clave es la funcion max
+print(obtain_max([1, 45, 87, 21, 0, 23, 28]))
--- 1b/ej1b4.py
+++ 1b/ej1b4.py
@@ -29,45 +29,16 @@ Ejemplo:
     Average: 19.74
     Standard deviation: 26.03
 
-Enunciat:
-
-Implementar la funció 'results(list_numbers)' que rebi com a paràmetre una
-llista que conté nombres enters i decimals anomenada 'list_numbers', es demana
-calcular la mitjana i la desviació estàndard mitjançant la
-llibreria "Numpy" de tots els números dins de la llista.
-
-Cada resultat s'ha d'imprimir en pantalla amb el nom respectiu
-'Average: {value}' i 'Standard deviation: {value}', aquests resultats deuen
-estar arrodonits a 2 decimals.
-
-Els resultats també han de ser retornats per la funció.
-
-Pots consultar la referència de la llibreria 'Numpy' en el següent enllaç:
-https://numpy.org/doc/stable/reference/index.html#reference
-
-En concret, et recomanem que consultis:
-* https://numpy.org/doc/stable/reference/generated/numpy.mean.html
-* https://numpy.org/doc/stable/reference/generated/numpy.std.html#numpy.std
-
-Paràmetre:
-- list_numbers: Llista de nombres enters i decimals.
-
-Exemple:
-     Entrada:
-     [1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42]
-    
-     Sortida:
-     Average: 19.74
-     Standard deviation: 26.03
 '''
-
-
 import numpy as np
 
 def results(list_numbers):
-    # Write here your code
-    pass
+    average = np.mean(list_numbers)
+    standard = np.std(list_numbers)
+    print("Average: ", {round(average, 2)})
+    print("Standard deviation: ", {round(standard, 2)})
+    return average, standard
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# results([1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42])
+results([1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42])

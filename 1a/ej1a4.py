e5cea2eb66910234246bfcbd489efa51a14ab8a9
carloslario <carloslariogomez@gmail.com>
2025-12-10 19:51:40 +0100

    ejercicio 4 subido

--- 1a/ej1a4.py
+++ 1a/ej1a4.py
@@ -14,29 +14,16 @@ Ejemplo:
 
     Salida:
     9
-
-Enunciat:
-Crea una funció anomenada "count_vowels(text_chain)" que rebi com a paràmetre
-una cadena de text de tipus string anomenada 'text_chain' i retorni el número
-de vocals, ja siguin majúscules o minúscules, sense accent, que es trobin en 
-aquesta cadena de text.
-
-Paràmetres:
-- text_chain: Aquest paràmetre només admet strings.
-
-Exemple:
-     Entrada:
-     count_vowels('Hello world, this is an example.')
-
-     Sortida:
-     9
-
 '''
 
 def count_vowels(text_chain:str):
-    # Write here your code
-    pass
+    count = 0
+    vowel_list = "AEIOUaeiou"
+    for n in text_chain:
+        if n in vowel_list:
+            count += 1
+    return count
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(count_vowels("Hello world, this is an example."))
+# la clave está en crear una lista auxiliar que tenga las vocales e ir comparandola con cada elemento del texto
+print(count_vowels("Hello world, this is an example."))

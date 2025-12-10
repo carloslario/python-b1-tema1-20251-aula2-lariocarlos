71b3f68ef754005c7d8ececaefd4d65eda4158c8
carloslario <carloslariogomez@gmail.com>
2025-12-10 19:48:52 +0100

    ejercicio 3 subido

--- 1a/ej1a3.py
+++ 1a/ej1a3.py
@@ -14,27 +14,12 @@ Ejemplo:
     Salida:
     !dlrow olleH
 
-Enunciat:
-Implementa una funció anomenada "invert_text(text_chain)" que rebi com
-paràmetre una cadena de text de tipus string anomenada 'text_chain' i torni
-el text invertit.
-
-Paràmetres:
-- text_chain: Aquest paràmetre només admet strings.
-
-Exemple:
-     Entrada:
-     invert_text('Hello world!')
-
-     Sortida:
-     !dlrow olleH
 
 '''
 
 def invert_text(text_chain:str):
-    # Write here your code
-    pass
+    return text_chain[::-1]
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(invert_text("Hello world!"))
+# es sencillo utilizando las propiedades de slicing de cadenas de texto o listas
+print(invert_text("Hello world!"))
